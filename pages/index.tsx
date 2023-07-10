dd790f819936620dd4e0ed2493bcaed5480bec84
yazoo-hero <69202927+yazoo-hero@users.noreply.github.com>
2023-07-10 12:58:52 +0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,65 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState} from "react";
+import styles from "./index.module.css";
+
+type Props = {
+    initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     fetchImage().then((newImage) => {
+    //         setImageUrl(newImage.url);
+    //         setLoading(false);
+    //     });
+    // }, []);
+
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    }
+
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}> view other cats </button>
+            <div className={styles.frame}> {loading || <img src={imageUrl} className={styles.img} />} </div>
+        </div>
+    )
+};
+
+
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+type Image = {
+    url: string;
+};
+
+const isImage = (value: unknown): value is Image => {
+    if(!value || typeof value !=="object") {
+        return false
+    }
+
+    return "url" in value && typeof value.url === "string";
+}
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0]
+};
+
